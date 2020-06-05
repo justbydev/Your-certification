305373ed932409ff576ca8742183e359c47c14f5
sgu960323 <wjhappyboy@hanmail.net>
2020-06-05 15:29:57 +0900

    login activity

--- app/src/main/java/com/example/coolpiece/LoginActivity.java
+++ app/src/main/java/com/example/coolpiece/LoginActivity.java
@@ -1,19 +1,97 @@
 package com.example.coolpiece;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends Fragment {
-    @Nullable
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonsignup;
+    FirebaseAuth firebaseAuth;
+    TextView loginfailmessage;
+    TextView textViewsignin;
+    ProgressDialog progressDialog;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.login, container, false);
-        return v;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        buttonsignup=(Button)findViewById(R.id.buttonSignup);
+        loginfailmessage=(TextView)findViewById(R.id.loginfailmessage);
+        textViewsignin=(TextView)findViewById(R.id.textViewSignin);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonsignup.setOnClickListener(buttononclicklistener);
+        textViewsignin.setOnClickListener(buttononclicklistener);
+    }
+
+    private View.OnClickListener buttononclicklistener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id=v.getId();
+            switch(id){
+                case R.id.buttonSignup:
+                    userLogin();
+                    return;
+                case R.id.textViewSignin:
+                    Intent intent=new Intent(LoginActivity.this, SignupActivity.class);
+                    startActivity(intent);
+                default:
+                    return;
+            }
+        }
+    };
+
+    public void userLogin(){
+        String email=editTextEmail.getText().toString();
+        String password=editTextPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "email을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "password를 입력해 주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("로그인중입니다. 잠시 기다려 주세요...");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Intent intent=new Intent(LoginActivity.this, MainFragment.class);
+                            finish();
+                            startActivity(intent);
+                        }
+                        else{
+                            loginfailmessage.setText("이메일/비밀번호를 확인해주세요.");
+                        }
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/coolpiece/MainFragment.java
+++ app/src/main/java/com/example/coolpiece/MainFragment.java
@@ -1,51 +1,82 @@
 package com.example.coolpiece;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainFragment extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
+
     HomeActivity homeActivity=null;
     MypageActivity mypageActivity=null;
-    LoginActivity loginActivity=null;
+    TextView menu_home;
+    TextView menu_mypage;
+    TextView menu_log;
+    FirebaseAuth firebaseAuth;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frag_main);
 
-        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation);
+
+        menu_home=(TextView)findViewById(R.id.menu_home);
+        menu_mypage=(TextView)findViewById(R.id.menu_mypage);
+        menu_log=(TextView)findViewById(R.id.menu_log);
+
+        menu_home.setOnClickListener(buttononclicklistener);
+        menu_mypage.setOnClickListener(buttononclicklistener);
+        menu_log.setOnClickListener(buttononclicklistener);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser()!=null){
+            menu_log.setText("로그아웃");
+        }
+        else{
+            menu_log.setText("로그인");
+        }
 
         homeActivity=new HomeActivity();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, homeActivity).commit();
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int select=item.getItemId();
-                switch(select){
-                    case R.id.menu_home:
-                        if(homeActivity==null){
-                            homeActivity=new HomeActivity();
-                            getSupportFragmentManager().beginTransaction().add(R.id.container, homeActivity).commit();
-                        }
-                        else{
-                            getSupportFragmentManager().beginTransaction().show(homeActivity).commit();
-                        }
-                        if(mypageActivity!=null){
-                            getSupportFragmentManager().beginTransaction().hide(mypageActivity).commit();
-                        }
-                        if(loginActivity!=null){
-                            getSupportFragmentManager().beginTransaction().hide(loginActivity).commit();
-                        }
-                        return true;
-                    case R.id.menu_mypage:
+        getSupportFragmentManager().beginTransaction().add(R.id.container, homeActivity).commit();
+    }
+
+    private View.OnClickListener buttononclicklistener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id=v.getId();
+            switch(id){
+                case R.id.menu_log:
+                    Intent intent=new Intent(MainFragment.this, LoginActivity.class);
+                    startActivity(intent);
+                    return;
+                case R.id.menu_home:
+                    if(homeActivity==null){
+                        homeActivity=new HomeActivity();
+                        getSupportFragmentManager().beginTransaction().add(R.id.container, homeActivity).commit();
+                    }
+                    else{
+                        getSupportFragmentManager().beginTransaction().show(homeActivity).commit();
+                    }
+                    if(mypageActivity!=null){
+                        getSupportFragmentManager().beginTransaction().hide(mypageActivity).commit();
+                    }
+                    return;
+                case R.id.menu_mypage:
+                    if(firebaseAuth.getCurrentUser()!=null){
                         if(mypageActivity==null){
                             mypageActivity=new MypageActivity();
                             getSupportFragmentManager().beginTransaction().add(R.id.container, mypageActivity).commit();
@@ -56,29 +87,42 @@ public class MainFragment extends AppCompatActivity {
                         if(homeActivity!=null){
                             getSupportFragmentManager().beginTransaction().hide(homeActivity).commit();
                         }
-                        if(loginActivity!=null){
-                            getSupportFragmentManager().beginTransaction().hide(loginActivity).commit();
-                        }
-                        return true;
-                    case R.id.menu_log:
-                        if(loginActivity==null){
-                            loginActivity=new LoginActivity();
-                            getSupportFragmentManager().beginTransaction().add(R.id.container, loginActivity).commit();
-                        }
-                        else{
-                            getSupportFragmentManager().beginTransaction().show(loginActivity).commit();
-                        }
-                        if(homeActivity!=null){
-                            getSupportFragmentManager().beginTransaction().hide(homeActivity).commit();
-                        }
-                        if(mypageActivity!=null){
-                            getSupportFragmentManager().beginTransaction().hide(mypageActivity).commit();
-                        }
-                        return true;
-                    default:
-                        return true;
-                }
+                    }
+                    else{
+                        AlertDialog.Builder builder= new AlertDialog.Builder(MainFragment.this);
+                        builder.setTitle("로그인이 필요합니다.")
+                                .setMessage("로그인 화면으로 이동하시겠습니까?")
+                                .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent=new Intent(MainFragment.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if(homeActivity==null){
+                                            homeActivity=new HomeActivity();
+                                            getSupportFragmentManager().beginTransaction().add(R.id.container, homeActivity).commit();
+                                        }
+                                        else{
+                                            getSupportFragmentManager().beginTransaction().show(homeActivity).commit();
+                                        }
+                                        if(mypageActivity!=null){
+                                            getSupportFragmentManager().beginTransaction().hide(mypageActivity).commit();
+                                        }
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alertDialog=builder.create();
+                        alertDialog.show();
+                    }
+
+                    return;
+                default:
+                    return;
             }
-        });
-    }
+        }
+    };
 }
